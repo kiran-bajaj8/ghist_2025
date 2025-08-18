5635068f53f120feb56924bd9c43dc26e04b41a9
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-08-18 19:37:13 -0400

    Update vcf2sfs.sh

--- bottleneck/vcf2sfs.sh
+++ bottleneck/vcf2sfs.sh
@@ -4,8 +4,12 @@ module load bcftools/1.21
 # Processing and Extracting from the VCF
 bcftools view -m 2 -M 2 -v snps GHIST_2025_bottleneck.testing.vcf.gz -o GHIST-bottleneck.snps.vcf.gz
 
-# 
+# Creating a Popfile
+## display all the samples in the VCF file
 bcftools query -l GHIST-bottleneck.snps.vcf.gz
+
+# Generating an SFS Using easySFS
+easySFS.py -i GHIST-bottleneck.snps.vcf.gz -p GHIST-bottleneck.popfile --proj 40 -o GHIST_bottleneck_sfs -a --unfold
 
 
 
