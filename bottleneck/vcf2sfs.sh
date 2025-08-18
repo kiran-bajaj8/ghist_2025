3e667c5835177b7b1e772281234ff44fde31fa04
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-08-18 19:15:45 -0400

    Create vcf2sfs.sh

---
+++ bottleneck/vcf2sfs.sh
@@ -0,0 +1,11 @@
+# load modules
+module load bcftools/1.21
+
+# Processing and Extracting from the VCF
+bcftools view -m 2 -M 2 -v snps GHIST_2025_bottleneck.testing.vcf.gz -o GHIST-bottleneck.snps.vcf.gz
+
+#
+bcftools query -l GHIST-bottleneck.snps.vcf.gz
+
+
+
