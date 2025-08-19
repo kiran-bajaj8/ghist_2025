298e17655a02c2e80917c0e9219055cd1f5a343d
Madeline G. Eppley <90652097+madeline-eppley@users.noreply.github.com>
2025-08-18 20:22:26 -0400

    Update vcf2sfs.R

--- bottleneck/vcf2sfs.R
+++ bottleneck/vcf2sfs.R
@@ -5,6 +5,11 @@ library(vcfR)
 # load in our biallelic filtered file - i already filtered out the biallelic sites here using bcftools in the command line with the following
 # bcftools view -m 2 -M 2 -v snps GHIST_2025_bottleneck.testing.vcf.gz -o GHIST-bottleneck.snps.vcf.gz
 # bcftools query -l GHIST_2025_bottleneck.testing.vcf.gz
+
+# i was also able to figure out # of SNPs and # of chromosomes with the following 
+
+# (base) [eppley.m@c0499 ghist_2025]$ bcftools query -f '%CHROM\n' GHIST-bottleneck.snps.vcf.gz | sort | uniq -c
+# 716072 1
 vcf <- read.vcfR("/Users/madelineeppley/Desktop/GHIST-bottleneck.snps.vcf.gz")
 
 # get it into genlight to prep for dartR
